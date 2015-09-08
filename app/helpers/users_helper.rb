9cc0e2cff59bb51e4d8db4c45ee6e6ded3e867ab
parasquid <tristan.gomez@gmail.com>
2015-09-09 06:00:37 +0800

    fix sharing urls

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,16 +4,18 @@ module UsersHelper
   FACEBOOK_SHARE_URL = 'http://www.facebook.com/sharer/sharer.php'
 
   def tweet_link(referral_code, message)
-    url = CGI::escape root_url
-    ref = CGI::escape referral_code
+    url = CGI::escape referral_url(referral_code)
     text = CGI::escape message
-    link_to("#{TWITTER_SHARE_URL}?url=#{url}&ref=#{ref}&text=#{text}".html_safe, class: 'twit', target: '_blank') {}
+    link_to("#{TWITTER_SHARE_URL}?url=#{url}&text=#{text}".html_safe, class: 'twit', target: '_blank') {}
   end
 
   def fb_link(referral_code, message)
-    url = CGI::escape root_url
-    ref = CGI::escape referral_code
+    url = CGI::escape referral_url(referral_code)
     text = CGI::escape message
-    link_to("#{FACEBOOK_SHARE_URL}?u=#{url}&ref=#{ref}&title=#{text}".html_safe, class: 'fb', target: '_blank') {}
+    link_to("#{FACEBOOK_SHARE_URL}?u=#{url}&title=#{text}".html_safe, class: 'fb', target: '_blank') {}
+  end
+
+  def referral_url(referral_code)
+    "#{root_url}?ref_code=#{referral_code}"
   end
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,14 +1,21 @@
 require "rails_helper"
 
 RSpec.describe UsersHelper, :type => :helper do
-  let(:host) { CGI::escape 'http://test.host/' }
+  let(:host) { 'http://test.host/' }
   let(:referral_code) { 'abcd1234' }
+  let(:url) { CGI::escape "#{host}?ref_code=#{referral_code}" }
   let(:message) { 'message' }
+
+  describe "#referral_url" do
+    it 'returns the referral url for sharing' do
+      expect(helper.referral_url(referral_code)).to eq("http://test.host/?ref_code=#{referral_code}")
+    end
+  end
 
   describe "#tweet_link" do
     let(:share_url) { 'http://twitter.com/share' }
     it "returns a twitter link" do
-      href = "#{share_url}?url=#{host}&ref=#{referral_code}&text=#{message}"
+      href = "#{share_url}?url=#{url}&text=#{message}"
       expect(helper.tweet_link(referral_code, message)).to eq("<a class=\"twit\" target=\"_blank\" href=\"#{href}\"></a>")
     end
   end
@@ -16,8 +23,9 @@ RSpec.describe UsersHelper, :type => :helper do
   describe "#fb_link" do
     let(:share_url) { 'http://www.facebook.com/sharer/sharer.php' }
     it "returns an fb link" do
-      href = "#{share_url}?u=#{host}&ref=#{referral_code}&title=#{message}"
+      href = "#{share_url}?u=#{url}&title=#{message}"
       expect(helper.fb_link(referral_code, message)).to eq("<a class=\"fb\" target=\"_blank\" href=\"#{href}\"></a>")
     end
   end
+
 end
