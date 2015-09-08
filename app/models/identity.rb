a7dfd4d61d9dc5f286fbe4504509891955d5623f
parasquid <tristan.gomez@gmail.com>
2015-09-09 04:05:36 +0800

    add in fb js code

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,3 +1,16 @@
 class Identity < ActiveRecord::Base
   belongs_to :user
+  validates_presence_of :uid, :provider
+  validates_uniqueness_of :uid, :scope => :provider
+
+  def self.create_from_oauth(auth)
+    create! do |identity|
+      identity.provider = auth['provider']
+      identity.uid = auth['uid']
+    end
+  end
+
+  def self.find_from_oauth(auth)
+    where(provider: auth['provider'], uid: auth['uid'].to_s).first
+  end
 end
--- spec/models/identity_spec.rb
+++ spec/models/identity_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Identity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "initialization" do
+    it 'creates an identity given a provider and a uid'
+    it 'retrieves an identity given a provider and a uid'
+  end
+
+  describe "validations" do
+    it 'validates the presence of a provider'
+    it 'validates the presence of a uid within the scope of a provider'
+  end
 end
